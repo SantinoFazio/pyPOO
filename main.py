a414e1792cc57a7a166977284060f3108933a47b
SantinoFazio <115169486+SantinoFazio@users.noreply.github.com>
2023-10-02 15:04:46 +0000

    add:code
    clase con metodos

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 class Automovil:
   ruedas=4
-  def __init__(self,color,marca,aceleracion,velocidad) :
+  def __init__(self,color,marca,aceleracion) :
     self.color=color
     self.marca=marca
     self.aceleracion=aceleracion
     self.velocidad=0
-  
-  print(ruedas)
+
+auto=Automovil("rojo","bmw",70)
+print("aceleracion ", auto.aceleracion)
+print("ruedas ",auto.ruedas)
+
+#mod aceleracion
+auto.aceleracion=50
+print(auto.aceleracion)
+
+def acelerar():
+  auto.velocidad+=auto.aceleracion
+  print("velocidad aumenta a:" ,auto.velocidad)
+def frenar():
+  auto.velocidad-=auto.aceleracion
+  print("velocidad baja a:" ,auto.velocidad)  
+
+print("aceleraa:")
+acelerar()
+print("frenaa:")
+frenar()
+print("frenaa:")
+frenar()
