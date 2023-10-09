8b8f662e297f5a0e113cdeec3d547d46efd2e290
SantinoFazio <santino.fazio@davinci.edu.ar>
2023-10-09 13:33:16 +0000

    version 1.1

---
+++ clases/Automovil.py
@@ -0,0 +1,30 @@
+from clases.Transporte import Transporte
+from clases.Vehiculo import Vehiculo
+from abc import ABC, abstractmethod 
+
+class Automovil(Transporte,ABC,Vehiculo):
+    ruedas = 4
+
+    def __init__(self, color, marca, aceleracion,_ano,__modelo):
+        super().__init__(_ano,__modelo)
+        self.color = color
+        self.marca = marca
+        self.aceleracion = aceleracion
+        self.velocidad = 0
+        self.__modelo= __modelo
+        self._ano= _ano
+
+    def acelerar(self):
+        self.velocidad += self.aceleracion
+
+    def frenar(self):
+        self.velocidad -= self.aceleracion
+
+
+    def conducir(self):
+        print("Conduciendo automóvil")
+
+    def volar(self):
+        print("volando")
+        pass  # No se puede volar en un automóvil
+  
--- main.py
+++ main.py
@@ -1,85 +1,10 @@
-from abc import ABC, abstractmethod
+from clases.Automovil import Automovil 
+from clases.AutomovilVolador import AutomovilVolador 
+auto = Automovil("rojo", "bmw", 70,2022,"3.4")
 
-class Transporte(ABC):
-    @abstractmethod
-    def conducir(self):
-        pass
-
-    @abstractmethod
-    def volar(self):
-        pass
-
-class Automovil(Transporte):
-    ruedas = 4
-
-    def __init__(self, color, marca, aceleracion):
-        self.color = color
-        self.marca = marca
-        self.aceleracion = aceleracion
-        self.velocidad = 0
-
-    def acelerar(self):
-        self.velocidad += self.aceleracion
-
-    def frenar(self):
-        self.velocidad -= self.aceleracion
-
-
-    def conducir(self):
-        print("Conduciendo automóvil")
-
-    def volar(self):
-        print("volando")
-        pass  # No se puede volar en un automóvil
-  
-auto = Automovil("rojo", "bmw", 70)
-auto2 = Automovil("gris", "bmw", 80)
-
-print("Aceleracion:", auto.aceleracion)
-print("Ruedas:", auto.ruedas)
-
-# Modify acceleration auto 1
-auto.aceleracion = 50
-print("----------AUTO 1:--------------", auto.aceleracion)
-auto.conducir()
-print('Auto 1 velocidad de:', auto.velocidad)
-auto.acelerar()
-print('Auto 1 velocidad de:', auto.velocidad)
-auto.acelerar()
-print('Auto 1 velocidad de:', auto.velocidad)
-auto.frenar()
-print('Auto 1 velocidad de:', auto.velocidad)
-
-# Modify acceleration auto 2
-auto2.aceleracion = 150
-print("-------------AUTO 2:--------------", auto2.aceleracion)
-auto2.conducir()
-print('Auto 2 velocidad de:', auto2.velocidad)
-auto2.acelerar()
-print('Auto 2 velocidad de:', auto2.velocidad)
-auto2.frenar()
-print('Auto 2 velocidad de:', auto2.velocidad)
-auto2.frenar()
-print('Auto 2 velocidad de:', auto2.velocidad)
+print(auto._ano)
+auto.set_ano(2200)
+print(auto._ano)
 
 
 
-
-class AutomovilVolador(Automovil):
-    ruedas = 6
-
-
-    def __init__(self, color, marca, aceleracion, estaVolando=True):
-        super().__init__(color, marca, aceleracion)
-        self.estaVolando = estaVolando
-
-    def vuela(self):
-        self.estaVolando = True
-
-    def noVuela(self):
-        self.estaVolando = False
-
-    
-print("---------AUTO VOLADOR----------")
-volador = AutomovilVolador("rojo", "ford", 90.0, False)  # Aceleración en decimal
-volador.volar()
